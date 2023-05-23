21126c8de627df721e59272f105209dcd46742a5
Azsolti <89606115+Azsolti@users.noreply.github.com>
2023-05-23 14:02:28 +0200

    Update kieg.py

--- kieg.py
+++ kieg.py
@@ -1,7 +1,7 @@
 import random
 
 arr = "ABCDEFGHIJKLMNOPQRSTUVWXY"
-arr_2 = "DEABCFGHIJKLMNOPQRSTUVWXY"
+arr_2 = "ABHDEFGMIJKLRNOPQWSTUVCXY"
 matrix = [[arr[i*5 + j] for j in range(5)] for i in range(5)]
 matrix_2 = [[arr_2[i*5 + j] for j in range(5)] for i in range(5)]
 
@@ -15,18 +15,19 @@ def moves():
     for i, row in enumerate(matrix_2, start=0):
         for ii, char in enumerate(row,start=0): 
             indexes_2[char] = i,ii
-            
-        
+
     for k,v in indexes.items():
         if indexes_2[k] == v:
-            
             pass
-            
+        
+        elif indexes_2[k][0] == v[0]:
+                print("This should move left or right")
+        
         else:
-            res = indexes_2[k][1] - v[1]
-            print(res)
-            print("---------------")
+            print("This should move towards top or bottom")
             print(indexes[k])
-            print("---------------")
             print(indexes_2[k])
+            print("---------------")
             
+      
+moves()
